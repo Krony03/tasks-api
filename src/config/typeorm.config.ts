dfba49b6860257967ee97e59b79d9df07b4d08d8
Matheus Souza <matheus.fragamfs@gmail.com>
2022-03-19 15:41:14 -0300

    :wrench: docker added for developing database
    Signed-off-by: Matheus Souza <matheus.fragamfs@gmail.com>

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'db.bwpcmajzvqesrlyiiylh.supabase.co',
-  port: 5432,
-  username: 'postgres',
-  password: 'Q*9wwqF%WDfV',
-  database: 'postgres',
+  host: process.env.PG_DB_HOST || '0.0.0.0',
+  port: Number(process.env.PG_DB_PORT) || 5432,
+  username: process.env.PG_DB_USERNAME || 'pguser',
+  password: process.env.PG_DB_PASSWORD || 'pgpassword',
+  database: process.env.PG_DB_NAME || 'postgres',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
