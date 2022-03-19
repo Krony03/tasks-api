dc9a2d585cd42d55847fc004f30a8bbc2f49ae60
Matheus Souza <matheus.fragamfs@gmail.com>
2022-03-19 14:50:46 -0300

    :wrench: env attributes changed
    Signed-off-by: Matheus Souza <matheus.fragamfs@gmail.com>

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: 'db.bwpcmajzvqesrlyiiylh.supabase.co',
+  port: 5432,
+  username: 'postgres',
+  password: 'Q*9wwqF%WDfV',
+  database: 'postgres',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
